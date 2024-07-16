76718f43ef4706dc01a9c8668c1ff8b8724977a7
Eva <eva.aglarsen@gmail.com>
2024-07-16 06:13:52 -0600

    licencing stuff

--- glslwindow.py
+++ glslwindow.py
@@ -1,5 +1,24 @@
+licence = r"""
+Copyright 2024 Calvin Larsen:
+                    GNU AFFERO GENERAL PUBLIC LICENSE
+                       Version 3, 19 November 2007
+
+ Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+ Everyone is permitted to copy and distribute verbatim copies
+ of this license document, but changing it is not allowed.
+
+                            Preamble
+
+  The GNU Affero General Public License is a free, copyleft license for
+software and other kinds of works, specifically designed to ensure
+cooperation with the community in the case of network server software.
+
+Rest in ./LICENCE
+https://github.com/Cgamess/glslcode
+""" 
+if 0: print(license) # assigned it to a var and put it in a print statement to keep it in the code after compilation
 import os
-os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = r"glslcode: https://github.com/Cgamess/glslcode"
 import pygame
 from OpenGL.GL import *
 from OpenGL.GL.shaders import compileProgram, compileShader
@@ -9,13 +28,11 @@ from tkinter import filedialog
 import numpy as np
 import time
 import json
-
+tkroot = tk.Tk()
+tkroot.withdraw()  # Hide the main window
 def open_file_dialog(types=[("PNG files", "*.png")], title="Image"):
-    root = tk.Tk()
-    root.withdraw()  # Hide the main window
     
     file_path = filedialog.askopenfilename(filetypes=types, title=title)
-    root.destroy()   # Destroy the Tkinter window after file selection
     
     return file_path
 
